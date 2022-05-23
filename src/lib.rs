8ed79793bafe8f4ab40c878e0e2f74f6f6677914
jb-alvarado <jb@pixelcrusher.de>
2022-05-23 15:46:17 +0200

    allow recursive folder copy

--- src/lib.rs
+++ src/lib.rs
@@ -1,74 +1,99 @@
 extern crate multiqueue2 as multiqueue;
 
 use std::{
-    fs::File,
+    fs::{self, File},
     io::{self, BufReader, BufWriter, Read, Write},
-    path::{Path, PathBuf},
+    path::PathBuf,
     process::exit,
     thread,
     time::{Duration, SystemTime, UNIX_EPOCH},
 };
 
 use multiqueue::{broadcast_queue, BroadcastReceiver};
+use pathdiff::diff_paths;
 use question::{Answer, Question};
+use walkdir::WalkDir;
 
-pub fn validate(inputs: &Vec<String>, outputs: &Vec<String>) -> Result<(), &'static str> {
-    let mut override_all = false;
+#[derive(Debug)]
+struct Source {
+    name: Option<String>,
+    path: PathBuf,
+}
 
-    if inputs.len() > 1 {
-        for out in outputs {
-            if Path::new(&out).is_file() {
-                return Err("Only one input and multiple output files, or multiple inputs and output folders supported!");
+fn get_input_paths(inputs: &Vec<String>) -> Vec<Source> {
+    let mut file_list = vec![];
+
+    for input in inputs {
+        let path = PathBuf::from(&input);
+
+        if path.is_file() {
+            file_list.push(Source { name: None, path })
+        } else {
+            let mut root = path.clone();
+
+            if let Some(r) = path.parent() {
+                root = r.to_path_buf();
+            };
+
+            for entry in WalkDir::new(&path)
+                .into_iter()
+                .flat_map(|e| e.ok())
+                .filter(|f| f.path().is_file())
+            {
+                let p = entry.path().to_path_buf();
+
+                if let Some(r) = p.parent().and_then(|f| diff_paths(f, &root)) {
+                    file_list.push(Source {
+                        name: Some(r.display().to_string()),
+                        path: entry.path().to_path_buf(),
+                    });
+                }
             }
         }
     }
 
-    for out in outputs {
-        let out_path = Path::new(&out);
-        if !override_all && out_path.is_file() {
-            let answer = Question::new(
-                format!("Output file \"{out}\" exists! Override all? [y/n]").as_str(),
-            )
-            .yes_no()
-            .until_acceptable()
-            .ask();
+    file_list
+}
 
-            if answer == Some(Answer::NO) {
-                return Err("Cancel copy process.");
-            }
+pub fn validate(inputs: &Vec<String>, outputs: &Vec<String>) -> Result<(), &'static str> {
+    let mut override_all = false;
+    let file_list = get_input_paths(inputs);
+    let input_len = file_list.len();
 
-            override_all = true;
-        }
+    for input in file_list {
+        let input_name = input.path.file_name().unwrap();
 
-        if out_path.is_dir() {
-            for input in inputs {
-                let in_path = Path::new(input);
+        for out in outputs {
+            let out_path = PathBuf::from(&out);
+            let mut path = out_path.clone();
 
-                if !in_path.is_file() {
-                    return Err("Only existing files as input are supported!");
-                }
+            if out_path.is_dir() {
+                if let Some(n) = input.name.clone() {
+                    path = out_path.join(n.clone());
+                };
 
-                let input_name = in_path.file_name().unwrap();
-                let out_file = out_path.join(input_name);
+                path = path.join(input_name);
+            } else if input_len > 1 {
+                return Err("Multiple input files to output file path, not supported!\nUse different output folders instead.");
+            };
 
-                if !override_all && out_file.is_file() {
-                    let answer = Question::new(
-                        format!(
-                            "Output file \"{}\" exists! Override all? [y/n]",
-                            out_file.display()
-                        )
-                        .as_str(),
+            if !override_all && path.is_file() {
+                let answer = Question::new(
+                    format!(
+                        "Output file \"{}\" exists! Override all? [y/n]",
+                        path.display()
                     )
-                    .yes_no()
-                    .until_acceptable()
-                    .ask();
+                    .as_str(),
+                )
+                .yes_no()
+                .until_acceptable()
+                .ask();
 
-                    if answer == Some(Answer::NO) {
-                        return Err("Cancel copy process.");
-                    }
-
-                    override_all = true;
+                if answer == Some(Answer::NO) {
+                    return Err("Cancel copy process.");
                 }
+
+                override_all = true
             }
         }
     }
@@ -100,24 +125,31 @@ pub fn copy_process(
     outputs: Vec<String>,
     show_progress: bool,
 ) -> Result<(), std::io::Error> {
-    for input in inputs {
+    let file_list = get_input_paths(&inputs);
+
+    for input in file_list {
         let mut buffer = [0; 65536];
         let mut counter = 0;
         let (send, recv) = broadcast_queue(92);
-        let input_file = Path::new(&input);
-        let input_name = input_file.file_name().unwrap();
-        let file_size = input_file.metadata()?.len();
-        let file = File::open(input_file)?;
+        let input_name = input.path.file_name().unwrap();
+        let file_size = input.path.metadata()?.len();
+        let file = File::open(&input.path)?;
         let mut input_reader = BufReader::new(file);
         let mut threads = vec![];
 
         for out in outputs.clone() {
-            let out_str = out.as_str();
-            let out_path = PathBuf::from(out_str);
+            let out_path = PathBuf::from(&out);
             let mut path = out_path.clone();
 
             if out_path.is_dir() {
-                path = out_path.join(input_name);
+                if let Some(n) = input.name.clone() {
+                    path = out_path.join(n.clone());
+                    if !path.is_dir() {
+                        fs::create_dir_all(&path)?;
+                    }
+                };
+
+                path = path.join(input_name);
             };
             let cur_recv = recv.add_stream();
             let t = thread::spawn(move || writer(path, cur_recv));
